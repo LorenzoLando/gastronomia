91ffb76de8387e1dc70190fee78c8df95369240f
Lorenzo <lorenzo.lando85@gmail.com>
2018-06-11 18:14:36 +0100

    added modal

---
+++ app/assets/scripts/modal.js
@@ -0,0 +1,20 @@
+
+var modal = document.querySelector(".modal");
+    var trigger = document.querySelectorAll(".trigger");
+    var closeButton = document.querySelector(".close-button");
+
+    function toggleModal() {
+        modal.classList.toggle("show-modal");
+    }
+
+    function windowOnClick(event) {
+        if (event.target === modal) {
+            toggleModal();
+        }
+    }
+//uso un for loop per accedere a tutti i bottono trigger
+    for(var i = 0; i < trigger.length; i++) {
+        trigger[i].addEventListener("click", toggleModal);    
+    }
+     closeButton.addEventListener("click", toggleModal);
+    window.addEventListener("click", windowOnClick);
